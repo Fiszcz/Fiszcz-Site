c21f61ec6e5e32e8d0b164c0e9de3eba63f6d67f
Fiszcz <fiszczu@gmail.com>
2018-03-28 05:06:51 +0200

    Add links to documentations of projects

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+
 declare var jquery:any;
 declare var $ :any;
 
@@ -9,12 +11,24 @@ declare var $ :any;
 })
 export class ProjectsComponent implements OnInit {
 
-  constructor() { }
+  constructor(public sanitizer: DomSanitizer) { }
 
   ngOnInit() {
     $(document).ready(function(){
       $('.modal').modal();
     });
+
+    this.mapDocumentations = new Map();
+    this.mapDocumentations.set("youmap", "https://drive.google.com/file/d/1XI65v3jWKLsS3noeplaKJ1oIOaeh_Xue/preview");
+    this.mapDocumentations.set("portalpracy", "https://drive.google.com/file/d/1QVyFAMmTZGgMTZQRwn9FrgzZFN2GCGF9/preview");
+    this.mapDocumentations.set("generatorfraktali", "https://drive.google.com/file/d/1t653GZpU7Jks9iPQgSBs2BagKlOBOM8E/preview");
+  }
+
+  mapDocumentations;
+  documentation;
+
+  changeDocumentation = function (selected) {
+      this.documentation = this.mapDocumentations.get(selected);
   }
 
 }
