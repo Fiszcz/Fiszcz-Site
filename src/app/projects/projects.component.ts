ca6df2e7c2d5067103782b4f838309867a55f076
Fiszcz <fiszczu@gmail.com>
2018-03-29 02:51:09 +0200

    Improve links to documentations

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -19,6 +19,7 @@ export class ProjectsComponent implements OnInit {
     });
 
     this.mapDocumentations = new Map();
+
     this.mapDocumentations.set("youmap", "https://drive.google.com/file/d/1XI65v3jWKLsS3noeplaKJ1oIOaeh_Xue/preview");
     this.mapDocumentations.set("portalpracy", "https://drive.google.com/file/d/1QVyFAMmTZGgMTZQRwn9FrgzZFN2GCGF9/preview");
     this.mapDocumentations.set("generatorfraktali", "https://drive.google.com/file/d/1t653GZpU7Jks9iPQgSBs2BagKlOBOM8E/preview");
